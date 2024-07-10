0b8920780a7bc9f68437f588032cc0ee79451fa1
sreyas.S <sreyas.sai213@gmail.com>
2024-07-10 13:05:11 +0530

    my commits

--- src/screens/Home/Homepage.tsx
+++ src/screens/Home/Homepage.tsx
@@ -6,6 +6,7 @@ import CircularsComponent from "../../components/CircularsComponent";
 import AboutSchoolComponent from "../../components/AboutSchoolComponent";
 import FacilitiesComponent from "../../components/FacilitiesComponent";
 import AdmissionsComponent from "../../components/AdmissionsComponent";
+import "./Homepage.css";
 
 function Homepage() {
   return (
@@ -18,6 +19,211 @@ function Homepage() {
       <AboutSchoolComponent />
       <FacilitiesComponent />
       <AdmissionsComponent />
+
+      <div className="elementor-container elementor-column-gap-default" style={{ display: "flex", alignItems: "center", padding: "50px" }}>
+        <div className="elementor-column elementor-col-14 elementor-top-column elementor-element" >
+          <div className="elementor-widget-wrap elementor-element-populated">
+            <div className="elementor-element elementor-widget elementor-widget-image">
+              <div className="elementor-widget-container">
+                <figure className="wp-caption">
+                  <img decoding="async" src="https://littlerock.edu.in/wp-content/uploads/elementor/thumbs/book-pn8rmd8pmk8b9349fl471hrv4b6q3c2nkdnx2l7xis.png" title="book" alt="book" loading="lazy" style={{ height: "100px", width: "100px"}} />
+                  <figcaption className="widget-image-caption wp-caption-text" style={{ color: "#7F0000", backgroundColor: "#FFFFFF", fontFamily: "Montserrat", fontWeight: "bold", alignItems: "center" }}>Circulars</figcaption>
+                </figure>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="elementor-column elementor-col-14 elementor-top-column elementor-element" >
+          <div className="elementor-widget-wrap elementor-element-populated">
+            <div className="elementor-element elementor-widget elementor-widget-image">
+              <div className="elementor-widget-container">
+                <figure className="wp-caption">
+                  <img decoding="async" src="https://littlerock.edu.in/wp-content/uploads/elementor/thumbs/education-pn8rq8ctq1iqzdi6v98zaio31c51qjexfi7s3jhnxw.png" title="education" alt="education" loading="lazy" style={{ height: "100px", width: "100px" }} />
+                  <figcaption className="widget-image-caption wp-caption-text" style={{ color: "#7F0000", backgroundColor: "#FFFFFF", fontFamily: "Montserrat", fontWeight: "bold", alignItems: "center" }}>Message to Alumni</figcaption>
+                </figure>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="elementor-column elementor-col-14 elementor-top-column elementor-element elementor-element-d741f45" data-id="d741f45" >
+          <div className="elementor-widget-wrap elementor-element-populated">
+            <div className="elementor-element elementor-widget elementor-widget-image">
+              <div className="elementor-widget-container">
+                <figure className="wp-caption">
+                  <img decoding="async" src="https://littlerock.edu.in/wp-content/uploads/elementor/thumbs/online-learning-pn8rqgtdfjubvv5whuwmeyj8dszcntcigo35f154dw.png" title="online-learning" alt="online-learning" loading="lazy" style={{ height: "100px", width: "100px" }} />
+                  <figcaption className="widget-image-caption wp-caption-text" style={{ color: "#7F0000", backgroundColor: "#FFFFFF", fontFamily: "Montserrat", fontWeight: "bold", alignItems: "center" }}>Documentary</figcaption>
+                </figure>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="elementor-column elementor-col-14 elementor-top-column elementor-element">
+          <div className="elementor-widget-wrap elementor-element-populated">
+            <div className="elementor-element elementor-widget elementor-widget-image">
+              <div className="elementor-widget-container">
+                <figure className="wp-caption">
+                  <img decoding="async" src="https://littlerock.edu.in/wp-content/uploads/elementor/thumbs/idea-pn8rqp9x525wsctm4gk9jeedq9tnl3a3htyiqisktw.png" title="idea" alt="idea" loading="lazy" style={{ height: "100px", width: "100px" }} />
+                  <figcaption className="widget-image-caption wp-caption-text" style={{ color: "#7F0000", backgroundColor: "#FFFFFF", fontFamily: "Montserrat", fontWeight: "bold", alignItems: "center" }}>Our Founder</figcaption>
+                </figure>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="elementor-column elementor-col-14 elementor-top-column elementor-element" >
+          <div className="elementor-widget-wrap elementor-element-populated">
+            <div className="elementor-element elementor-widget elementor-widget-image">
+              <div className="elementor-widget-container">
+                <figure className="wp-caption">
+                  <img decoding="async" src="https://littlerock.edu.in/wp-content/uploads/elementor/thumbs/gears-pn8sfy7upuq6na5bovgi0j145vdlceijast1t3cvp0.png" title="gears" alt="gears" loading="lazy" style={{ height: "100px", width: "100px" }} />
+                  <figcaption className="widget-image-caption wp-caption-text" style={{ color: "#7F0000", backgroundColor: "#FFFFFF", fontFamily: "Montserrat", fontWeight: "bold", alignItems: "center" }}>Warning Against Fraud</figcaption>
+                </figure>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="elementor-column elementor-col-14 elementor-top-column elementor-element" >
+          <div className="elementor-widget-wrap elementor-element-populated">
+            <div className="elementor-element elementor-widget elementor-widget-image" >
+              <div className="elementor-widget-container">
+                <figure className="wp-caption">
+                  <img decoding="async" src="https://littlerock.edu.in/wp-content/uploads/elementor/thumbs/diploma-pn8sgsaosjvcysxmt8gk8bfv679c6pty2xol5y4a5w.png" title="diploma" alt="diploma" loading="lazy" style={{ height: "100px", width: "100px" }} />
+                  <figcaption className="widget-image-caption wp-caption-text" style={{ color: "#7F0000", backgroundColor: "#FFFFFF", fontFamily: "Montserrat", fontWeight: "bold", alignItems: "center" }}>Special Features</figcaption>
+                </figure>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="elementor-column elementor-col-14 elementor-top-column elementor-element" >
+          <div className="elementor-widget-wrap elementor-element-populated">
+            <div className="elementor-element elementor-widget elementor-widget-image">
+              <div className="elementor-widget-container">
+                <figure className="wp-caption">
+                  <img decoding="async" src="https://littlerock.edu.in/wp-content/uploads/elementor/thumbs/presentation-pn8si9vhm3wpckrt4dmaofx515x9dgqnaav9ipwub8.png" title="presentation" alt="presentation" loading="lazy" style={{ height: "100px", width: "100px" }} />
+                  <figcaption className="widget-image-caption wp-caption-text" style={{ color: "#7F0000", backgroundColor: "#FFFFFF", fontFamily: "Montserrat", fontWeight: "bold", alignItems: "center" }}> Achievements</figcaption>
+                </figure>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <section className="elementor-section elementor-top-section elementor-element elementor-section-boxed elementor-section-height-default elementor-section-height-default">
+        <div className="elementor-container elementor-column-gap-default">
+          <div className="elementor-column elementor-col-100 elementor-top-column elementor-element">
+            <div className="elementor-widget-wrap elementor-element-populated">
+              <div className="elementor-element elementor-widget elementor-widget-text-editor">
+                <div className="elementor-widget-container">
+                  <p><strong>Little Rock Indian School</strong>, a leading Educational Institution in the region was founded by Late Dr C.T. Abraham, who had extensive experience in the field of education in India and the USA. The natural aura of the beautiful serene, green campus blended with its passion to provide the best for the students to learn and explore has truly created a powerful atmosphere for everyone to nurture their skills and excel. LRIS shapes professionals who are an optimum mix of right attitude, skill, culture and knowledge.</p>						
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="elementor-section elementor-top-section elementor-element elementor-section-boxed elementor-section-height-default elementor-section-height-default">
+      <div className="elementor-container elementor-column-gap-default">
+        <div className="elementor-column elementor-col-50 elementor-inner-column elementor-element">
+          <div className="elementor-widget-wrap elementor-element-populated">
+            <div className="elementor-element elementor-widget elementor-widget-image">
+              <div className="elementor-widget-container">
+                <img decoding="async" src="https://littlerock.edu.in/wp-content/uploads/elementor/thumbs/DJI_0008-scaled-e1655705625448-pqkw5kmjexo4mx2rslf2dlk6bbi058sk1yeft7x9jo.jpg" title="default" alt="default" loading="lazy" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="elementor-column elementor-col-50 elementor-inner-column elementor-element">
+          <div className="elementor-widget-wrap elementor-element-populated" >
+            <div className="elementor-element elementor-widget elementor-widget-text-editor">
+              <div className="elementor-widget-container">
+                <p>To quote the words of the Founder, “This campus and everything that you see here is the gift of God. We are mere instruments in His hands.” <b>LITTLE ROCK</b> strives to provide the best possible facilities to its students, including a highly qualified teaching faculty, with the ultimate aim of providing an education of the highest order.</p>						</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </section>
+
+      <section className="elementor-section elementor-top-section elementor-element elementor-section-boxed elementor-section-height-default elementor-section-height-default">
+        <div className="elementor-container elementor-column-gap-default">
+          <div className="elementor-column elementor-col-50 elementor-top-column elementor-element" style={{padding:"10px"}}>
+            <div className="elementor-widget-wrap elementor-element-populated">
+              <div className="elementor-element elementor-widget elementor-widget-image">
+                <div className="elementor-widget-container">
+                  <img width="650" height="512" src="https://littlerock.edu.in/wp-content/uploads/2022/04/100A6551-768x512.jpg" alt="" />	
+                </div>
+              </div>
+              <div className="elementor-element elementor-element-e578cef elementor-widget elementor-widget-heading">
+                <div className="elementor-widget-container">
+                  <h5 className="elementor-heading-title elementor-size-default"><b>About Us</b></h5>		
+                </div>
+              </div>
+              <div className="elementor-element elementor-widget elementor-widget-text-editor">
+                <div className="elementor-widget-container">
+                  <p>The School was founded by Late Dr C.T. Abraham, who had extensive experience in the field of education in India and the USA. Though he is no more, the high ideals and principles he embodied are inherited by his successors. The management is composed of individuals with rich experience in the field of education both in India and abroad. Little Rock’s global vision of education is the obvious result of such an enlightened leadership</p>						</div>
+              </div>
+              </div>
+          </div>
+          <div className="elementor-column elementor-col-50 elementor-top-column elementor-element" style={{padding:"10px"}}>
+            <div className="elementor-widget-wrap elementor-element-populated">
+              <div className="elementor-element elementor-widget elementor-widget-image" border-radius="50px">
+                <div className="elementor-widget-container">
+                  <img width="650" height="512"  src="https://littlerock.edu.in/wp-content/uploads/2022/04/100A7243-1024x683.jpg" />													
+                  </div>
+              </div>
+              <div className="elementor-element elementor-element-3bc4a0b elementor-widget elementor-widget-heading">
+                <div className="elementor-widget-container">
+                  <h5 className="elementor-heading-title elementor-size-default"><b>Uniqueness of the School</b></h5>		
+                  </div>
+              </div>
+              <div className="elementor-element elementor-widget elementor-widget-text-editor">
+                <div className="elementor-widget-container">
+                  <p>Priority Highlands, the beautiful 30-acre campus, situated in Chanthar Village in Brahmavar, Udupi District is the home of LRIS. This campus is popularly known as “LITTLE ROCK”, and is 4 kms east of Brahmavar Bus Station on the Brahmavar-Hebri Road. Brahmavar is on National Highway No. 66, about 72 kms north of Mangalore. The nearest Airport is at Mangalore. The nearest Railway Station is in Udupi, about 20 kms from the Campus. Brahmavar lies between Udupi in the south and Kundapura in the north, on NH 66.</p>						</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="elementor-section elementor-top-section elementor-element elementor-section-boxed elementor-section-height-default elementor-section-height-default">
+        <div className="elementor-container elementor-column-gap-default">
+          <div className="elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-ffdab0f">
+            <div className="elementor-widget-wrap elementor-element-populated">
+              <div className="elementor-element elementor-element-bada6ae elementor-widget elementor-widget-heading">
+                <div className="elementor-widget-container">
+                  <h3 className="elementor-heading-title elementor-size-default"><b>Mission Statement</b></h3>		
+                  </div>
+              </div>
+              <div className="elementor-element elementor-element-960ae31 elementor-widget elementor-widget-text-editor">
+                <div className="elementor-widget-container">
+                  <p>The School Seeks to Provide Excellent Education with A View to Shaping God-Fearing, Patriotic And Responsible Citizens For The Future Of Our Country. The Children Who Are Educated Here Must Be Intellectually Robust, Morally Upright and Socially Sensitive. They Should Be an Asset to The Society.</p>						</div>
+              </div>
+            </div>
+          </div>
+          <div className="elementor-column elementor-col-50 elementor-top-column elementor-element">
+            <div className="elementor-widget-wrap elementor-element-populated">
+              <div className="elementor-element elementor-widget elementor-widget-heading">
+                <div className="elementor-widget-container">
+                  <h3 className="elementor-heading-title elementor-size-default"><b>Upcoming Events</b></h3>		
+                </div>
+              </div>
+              <section className="elementor-section elementor-inner-section elementor-element elementor-section-boxed elementor-section-height-default elementor-section-height-default">
+                <div className="elementor-container elementor-column-gap-default">
+                  <div className="elementor-column elementor-col-100 elementor-inner-column elementor-element">
+                    <div className="elementor-widget-wrap elementor-element-populated">
+                      <div className="elementor-element elementor-widget elementor-widget-text-editor">
+                        <div className="elementor-widget-container">
+                          <p>Kindergarten Sports Day</p><p>Commencement Day 2024</p><p>UKG Convocation 2024</p>						
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+      </section>
+
     </div>
   );
 }
